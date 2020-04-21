c86cfa5819695a4d5c04f4d1183bee121d8a1dae
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-04-21 10:42:24 +0100

    traffic lights change state at the right intervals

--- GameEngines2-LabTest-Connor Jepson/Assets/Scripts/TrafficLightStates.cs
+++ GameEngines2-LabTest-Connor Jepson/Assets/Scripts/TrafficLightStates.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class TrafficLightStates : MonoBehaviour
 {
-    private enum State
+    public enum State
     {
         Green,
         Yellow,
         Red,
     }
 
-    private State state;
+    public State state;
+
+    public float timer = 0f;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        var ObjRenderer = GetComponent<Renderer>();
+
         int randomState = Random.Range(0, 3);
         switch(randomState)
         {
             case 0:
+                ObjRenderer.material.SetColor("_Color", Color.green);
                 state = State.Green;
+                timer = Random.Range(5f, 10f);
                 break;
             case 1:
+                ObjRenderer.material.SetColor("_Color", Color.yellow);
                 state = State.Yellow;
+                timer = 4f;
                 break;
             case 2:
+                ObjRenderer.material.SetColor("_Color", Color.red);
                 state = State.Red;
+                timer = Random.Range(5f, 10f);
                 break;
         }
     }
@@ -33,26 +43,43 @@ public class TrafficLightStates : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch(state)
+        ChangeState();
+    }
+
+    void ChangeState()
+    {
+        var ObjRenderer = GetComponent<Renderer>();
+        timer -= Time.deltaTime;
+        if (timer <= 0)
         {
-            case State.Green:
-                Green();
-                break;
-            case State.Yellow:
-                Yellow();
-                break;
-            case State.Red:
-                Red();
-                break;
+            switch (state)
+            {
+                case State.Green:
+                    state = State.Yellow;
+                    ObjRenderer.material.SetColor("_Color", Color.yellow);
+                    timer = 4f;
+                    break;
+                case State.Yellow:
+                    state = State.Red;
+                    ObjRenderer.material.SetColor("_Color", Color.red);
+                    timer = Random.Range(5f, 10f); 
+                    break;
+                case State.Red:
+                    state = State.Green;
+                    ObjRenderer.material.SetColor("_Color", Color.green);
+                    timer = Random.Range(5f, 10f);
+                    break;
+            }
         }
     }
 
+    /*
     void Green()
     {
         var ObjRenderer = GetComponent<Renderer>();
         ObjRenderer.material.SetColor("_Color", Color.green);
 
-        StartCoroutine(Timer(2.0f, State.Yellow));
+        StartCoroutine(Timer(4.0f, State.Yellow));
     }
 
     void Yellow()
@@ -60,8 +87,7 @@ public class TrafficLightStates : MonoBehaviour
         var ObjRenderer = GetComponent<Renderer>();
         ObjRenderer.material.SetColor("_Color", Color.yellow);
 
-        //StartCoroutine("YellowTimer");
-        StartCoroutine(Timer(2.0f, State.Yellow));
+        StartCoroutine(Timer(4.0f, State.Red));
     }
 
     void Red()
@@ -69,34 +95,15 @@ public class TrafficLightStates : MonoBehaviour
         var ObjRenderer = GetComponent<Renderer>();
         ObjRenderer.material.SetColor("_Color", Color.red);
 
-        //StartCoroutine("RedTimer");
-        StartCoroutine(Timer(2.0f, State.Yellow));
+        StartCoroutine(Timer(4.0f, State.Green));
     }
 
-   /* IEnumerator GreenTimer()
-    {
-        float randomSec = Random.Range(5f, 10f);
-        yield return new WaitForSeconds(5f);
-        state = State.Yellow;
-    }
-
-    IEnumerator YellowTimer()
-    {
-        yield return new WaitForSeconds(4f);
-        state = State.Red;
-    }
-
-    IEnumerator RedTimer()
-    {
-        float randomSec = Random.Range(5f, 10f);
-        yield return new WaitForSeconds(5f);
-        state = State.Green;
-    }
-    */
+   
     IEnumerator Timer(float time, State stateNew)
     {
         yield return new WaitForSeconds(time);
         state = stateNew;
     }
+    */
     
 }
