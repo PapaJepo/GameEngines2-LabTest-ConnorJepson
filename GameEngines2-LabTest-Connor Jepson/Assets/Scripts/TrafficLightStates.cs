4867122502d8789485e088809b9f6d99b6759f9c
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-04-21 10:22:54 +0100

    Cylinders Spawn and Working on Colour States

---
+++ GameEngines2-LabTest-Connor Jepson/Assets/Scripts/TrafficLightStates.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightStates : MonoBehaviour
+{
+    private enum State
+    {
+        Green,
+        Yellow,
+        Red,
+    }
+
+    private State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randomState = Random.Range(0, 3);
+        switch(randomState)
+        {
+            case 0:
+                state = State.Green;
+                break;
+            case 1:
+                state = State.Yellow;
+                break;
+            case 2:
+                state = State.Red;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(state)
+        {
+            case State.Green:
+                Green();
+                break;
+            case State.Yellow:
+                Yellow();
+                break;
+            case State.Red:
+                Red();
+                break;
+        }
+    }
+
+    void Green()
+    {
+        var ObjRenderer = GetComponent<Renderer>();
+        ObjRenderer.material.SetColor("_Color", Color.green);
+
+        StartCoroutine(Timer(2.0f, State.Yellow));
+    }
+
+    void Yellow()
+    {
+        var ObjRenderer = GetComponent<Renderer>();
+        ObjRenderer.material.SetColor("_Color", Color.yellow);
+
+        //StartCoroutine("YellowTimer");
+        StartCoroutine(Timer(2.0f, State.Yellow));
+    }
+
+    void Red()
+    {
+        var ObjRenderer = GetComponent<Renderer>();
+        ObjRenderer.material.SetColor("_Color", Color.red);
+
+        //StartCoroutine("RedTimer");
+        StartCoroutine(Timer(2.0f, State.Yellow));
+    }
+
+   /* IEnumerator GreenTimer()
+    {
+        float randomSec = Random.Range(5f, 10f);
+        yield return new WaitForSeconds(5f);
+        state = State.Yellow;
+    }
+
+    IEnumerator YellowTimer()
+    {
+        yield return new WaitForSeconds(4f);
+        state = State.Red;
+    }
+
+    IEnumerator RedTimer()
+    {
+        float randomSec = Random.Range(5f, 10f);
+        yield return new WaitForSeconds(5f);
+        state = State.Green;
+    }
+    */
+    IEnumerator Timer(float time, State stateNew)
+    {
+        yield return new WaitForSeconds(time);
+        state = stateNew;
+    }
+    
+}
---
+++ GameEngines2-LabTest-Connor Jepson/Assets/Scripts/TrafficLights.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLights : MonoBehaviour
+{
+    public int numlights = 10;
+    public float radius = 10;
+
+    public List<GameObject> trafficlights;
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTrafficLights();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void SpawnTrafficLights()
+    {
+        trafficlights.Clear();
+        float thetaInc = (Mathf.PI * 2) / numlights;
+        for (int i = 0; i < numlights; i++)
+        {
+            float theta = i * thetaInc;
+            Vector3 pos = new Vector3(Mathf.Sin(theta) * radius, 0, Mathf.Cos(theta) * radius);
+            GameObject Cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            Cylinder.transform.position = pos;
+            Cylinder.name = "TrafficLight " + (i + 1);
+            trafficlights.Add(Cylinder);
+            Cylinder.gameObject.AddComponent<TrafficLightStates>();
+            Cylinder.transform.parent = this.transform;
+        }
+    }
+
+   
+
+    public void OnDrawGizmos()
+    {
+        float thetaInc = (Mathf.PI * 2) / numlights;
+        for (int i = 0; i < numlights; i++)
+        {
+            float theta = i * thetaInc;
+            Vector3 pos = new Vector3(Mathf.Sin(theta) * radius, 0, Mathf.Cos(theta) * radius);
+            Gizmos.DrawWireSphere(pos,2);
+        }
+    }
+}
